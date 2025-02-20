d6842e9219610bc882e6e861a60873a77f8d4b27
gabmartins <gabrielpassosmartins@gmail.com>
2025-02-20 11:15:14 -0300

    player crud

--- players/routers.py
+++ players/routers.py
@@ -1,11 +1,59 @@
+from uuid import UUID
+
 from ninja import Router
+from django.shortcuts import get_object_or_404
 
 from players.models import Player
-from players.serializers import PlayerSchema
+from players.serializers import PlayerResponse, PlayerCreate, PlayerIncrementStats
 
 router = Router()
 
 
-@router.get("/", response=list[PlayerSchema])
+@router.get("/", response=list[PlayerResponse])
 def get_players(request):
     return Player.objects.all()
+
+
+@router.get("/{player_id}", response=PlayerResponse)
+def get_player(request, player_id: UUID):
+    player = get_object_or_404(Player, id=player_id)
+    return player
+
+
+@router.post("/", response=PlayerResponse)
+def create_player(request, player_info: PlayerCreate):
+    player = Player.objects.create(**player_info.dict())
+    return player
+
+
+@router.put("/{player_id}", response=PlayerResponse)
+def update_player(request, player_id: UUID, player_info: PlayerCreate):
+    player = get_object_or_404(Player, id=player_id)
+    player_info = player_info.dict()
+
+    # check if there is any change
+    if all(getattr(player, attr) == value for attr, value in player_info.items()):
+        return
+
+    for attr, value in player_info.items():
+        setattr(player, attr, value)
+
+    player.save()
+    return player
+
+
+@router.put("/increment/{player_id}", response=PlayerResponse)
+def increment_player_stats(request, player_id: UUID, increment_info: PlayerIncrementStats):
+    player = get_object_or_404(Player, id=player_id)
+
+    for attr, value in increment_info.dict().items():
+        setattr(player, attr, getattr(player, attr) + value)
+
+    player.save()
+    return player
+
+
+@router.delete("/{player_id}")
+def delete_player(request, player_id: UUID) -> None:
+    player = get_object_or_404(Player, id=player_id)
+    player.delete()
--- players/serializers.py
+++ players/serializers.py
@@ -1,16 +1,46 @@
+from typing import Final
 from uuid import UUID
 
 from ninja import Schema
+from pydantic import field_validator
+
+from players.models import PlayerPosition
+
+DEFAULT_IMAGE_URL: Final = (
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKu1w7TulWMUKGszjJlb7PDtn0LVSJgGnrog&s"
+)
 
 
-class PlayerSchema(Schema):
-    id: UUID
+class _PlayerStats(Schema):
+    goals: int = 0
+    assists: int = 0
+    mvps: int = 0
+    yellow_cards: int = 0
+    red_cards: int = 0
+
+
+class _PlayerBase(_PlayerStats):
     name: str
     shirt_number: int
     position: str  # PlayerPosition
-    image_url: str | None = None
-    goals: int
-    assists: int
-    mvps: int
-    yellow_cards: int
-    red_cards: int
+    image_url: str | None = DEFAULT_IMAGE_URL
+
+
+class PlayerCreate(_PlayerBase):
+    @field_validator("position", mode="before")
+    @classmethod
+    def validate_position(cls, value):
+        value = value.lower()
+        try:
+            PlayerPosition(value)
+        except ValueError as e:
+            raise e
+        return value
+
+
+class PlayerResponse(_PlayerBase):
+    id: UUID
+
+
+class PlayerIncrementStats(_PlayerStats):
+    pass
